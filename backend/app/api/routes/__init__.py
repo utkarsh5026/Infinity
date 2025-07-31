17c6a66be5568d1df79efd582e6f76b31ec95f7d
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-31 20:42:27 +0530

    Update API routes in __init__.py to include authentication and user management
    - Added `auth_router`, `user_router`, and `card_router` to the API routes.
    - Updated the `__all__` attribute to reflect the new structure, enhancing modularity and accessibility of the routes.

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,4 +1,11 @@
-from .users import router as users_router
+from .auth import router as auth_router
 from .topics import router as topics_router
+from .user import router as user_router
+from .card import router as card_router
 
-__all__ = ["users_router", "topics_router"]
+__all__ = [
+    "auth_router",
+    "topics_router",
+    "user_router",
+    "card_router"
+]
