a33f5aca0aa64b9d41e2605279c78c96d3497cfd
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-07-31 20:43:46 +0530

    Update __init__.py to include SavedCard in exports
    - Added `SavedCard` to the `__all__` attribute in `__init__.py` to enhance module accessibility.
    - This change improves the modularity of the application by exposing the `SavedCard` class for use in other modules.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
 from .topic import Topic
-from .card import Card
+from .card import Card, SavedCard
 from .card_interaction import CardInteraction
 from .learning_session import LearningSession
 from .generation_cache import GenerationCache
@@ -8,6 +8,7 @@ from .generation_cache import GenerationCache
 __all__ = ["User",
            "Topic",
            "Card",
+           "SavedCard",
            "CardInteraction",
            "LearningSession",
            "GenerationCache"]
