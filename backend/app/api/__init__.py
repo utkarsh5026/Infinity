eff458bd9670eb4fa68ed26fb52442e2ba4ae49f
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-10-22 21:35:11 +0530

    Refactor __init__.py: remove unused route imports to simplify API router

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,16 +1,6 @@
 from fastapi import APIRouter
-from app.api.routes import (
-    auth_router,
-    topics_router,
-    user_router,
-    card_router,
-    explanation_router
-)
+from app.api.routes import auth_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router)
-api_router.include_router(topics_router)
-api_router.include_router(user_router)
-api_router.include_router(card_router)
-api_router.include_router(explanation_router)
