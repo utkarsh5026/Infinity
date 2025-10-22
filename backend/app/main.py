3be3d94b240f4b788642c71906917c0cb84484a5
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-10-22 21:42:14 +0530

    Add CORS middleware and update API router prefix in main.py
    - Added CORSMiddleware to handle cross-origin requests with configurable origins.
    - Updated the API router inclusion to use a prefix defined in settings for better organization and routing clarity.

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,16 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
-app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 @app.get("/")
