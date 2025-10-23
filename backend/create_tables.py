e55d348efcff1864df9f430d607301f7db6e9fb2
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-10-23 09:58:50 +0530

    Remove unused import of User model in create_tables.py

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -3,7 +3,7 @@ Script to create database tables
 """
 import asyncio
 from app.config.database import engine, Base
-from app.models.user import User  # Import all models here
+from app.models.user import User
 
 
 async def create_tables():
